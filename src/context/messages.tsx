f16aaad6570bde0f83b92f631064e3768200014e
Riciere Marcone <85197053+ricichien@users.noreply.github.com>
2023-05-02 18:29:33 -0300

    committing new files

---
+++ src/context/messages.tsx
@@ -0,0 +1,68 @@
+import { Message } from "@/lib/validators/message";
+import { nanoid } from "nanoid";
+import { ReactNode, createContext, useState } from "react";
+
+export const MessagesContext = createContext<{
+  messages: Message[];
+  isMessageUpdating: boolean;
+  addMessage: (message: Message) => void;
+  removeMessage: (id: string) => void;
+  updateMessage: (id: string, updateFn: (prevText: string) => string) => void;
+  setIsMessageUpdating: (isUpdating: boolean) => void;
+}>({
+  messages: [],
+  isMessageUpdating: false,
+  addMessage: () => {},
+  removeMessage: () => {},
+  updateMessage: () => {},
+  setIsMessageUpdating: () => {},
+});
+
+export function MessagesProvider({ children }: { children: ReactNode }) {
+  const [isMessageUpdating, setIsMessageUpdating] = useState<boolean>(false);
+  const [messages, setMessages] = useState<Message[]>([
+    {
+      id: nanoid(),
+      text: "Greetings! How can I help you?",
+      isUserMessage: false,
+    },
+  ]);
+
+  const addMessage = (message: Message) => {
+    setMessages((prev) => [...prev, message]);
+  };
+
+  const removeMessage = (id: string) => {
+    setMessages((prev) => prev.filter((message) => message.id !== id));
+  };
+
+  const updateMessage = (
+    id: string,
+    updateFn: (prevText: string) => string
+  ) => {
+    setMessages((prev) =>
+      prev.map((message) => {
+        if (message.id === id) {
+          return { ...message, text: updateFn(message.text) };
+        }
+
+        return message;
+      })
+    );
+  };
+
+  return (
+    <MessagesContext.Provider
+      value={{
+        messages,
+        isMessageUpdating,
+        addMessage,
+        removeMessage,
+        updateMessage,
+        setIsMessageUpdating,
+      }}
+    >
+      {children}
+    </MessagesContext.Provider>
+  );
+}
